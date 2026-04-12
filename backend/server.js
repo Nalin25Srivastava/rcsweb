4f23caa0e0d158705a5674285ddd73b7f9c7a523
Nalin Srivastava <rohan25srivastava@gmail.com>
2026-04-12 19:09:28 +0530

    Fix Vercel routing: move entry point to api/ folder and improve debug logging

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,11 @@ const limiter = rateLimit({
 });
 
 // Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.use(helmet());
 // app.use(limiter);
 app.use(cors());
